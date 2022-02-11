cdbec92e2842f4403a9e1784d3d5b3253cc74201
Michael Chuinard <mpchuinard@finlandia.edu>
2022-02-11 01:26:49 -0600

    added empty function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,18 +3,25 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 let basket = [];
 function addItem(item) {
-  basket.push(item);
+  for (let i = 0; i < item.length; i++)
+  basket.push(item[i]);
   if (item.length > 0) {
     }
     return true;
 }
 console.log(`Basket is ${basket}`);
-console.log('Adding milk (expect true)', addItem('milk'));
+console.log('Adding milk and eggs (expect true)', addItem(['milk']));
 console.log(`Basket is now ${basket}`);
 
-function listItems(moreItems) {
+function listItems() {
   for (let i = 0; i < basket.length; i++) {
-    }
-    return basket;
+  return basket[i];
+  }
 }
 console.log(listItems());
+
+function empty() {
+  basket.length = 0;
+  return basket;
+}
+console.log(empty());
