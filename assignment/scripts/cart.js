d3db9e619f3d3f6bcf63b3ad3bb2e219ccd05b93
Michael Chuinard <mpchuinard@finlandia.edu>
2022-02-10 22:42:15 -0600

    completed first part of assignment with addItem function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -1,3 +1,18 @@
 console.log('***** Cart Functions *****');
 // Make sure to test all functions here in the JS file!
 // We want to see how you are testing your code!!!
+let basket = [];
+function addItem(item) {
+for (let i = 0; i < item.length; i++) {
+  if (item.length > 0) {
+    basket.push(item[i]);
+  }
+}
+  return true;
+}
+console.log('Test - should say true:', addItem(['milk', 'eggs']));
+console.log(basket);
+
+function listItems() {
+  
+}
