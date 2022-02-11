6652fff334fc8cd59439b4f7a1350e00e71fba0d
Michael Chuinard <mpchuinard@finlandia.edu>
2022-02-11 16:53:10 -0600

    completed addItem function, listItems function, and empty function

--- assignment/scripts/cart.js
+++ assignment/scripts/cart.js
@@ -3,25 +3,24 @@ console.log('***** Cart Functions *****');
 // We want to see how you are testing your code!!!
 let basket = [];
 function addItem(item) {
-  for (let i = 0; i < item.length; i++)
-  basket.push(item[i]);
-  if (item.length > 0) {
-    }
-    return true;
+  basket.push(item);
+  return true;
 }
 console.log(`Basket is ${basket}`);
-console.log('Adding milk and eggs (expect true)', addItem(['milk']));
+console.log('Adding milk (expect true)', addItem('milk'));
+console.log('Adding bananas', addItem('bananas'));
+console.log('Adding blueberries', addItem('blueberries'));
 console.log(`Basket is now ${basket}`);
 
 function listItems() {
   for (let i = 0; i < basket.length; i++) {
-  return basket[i];
+    console.log(basket[i]);
+    }
   }
-}
-console.log(listItems());
+  listItems();
 
 function empty() {
   basket.length = 0;
   return basket;
 }
-console.log(empty());
+console.log('Basket is empty again (empty array):', empty());
